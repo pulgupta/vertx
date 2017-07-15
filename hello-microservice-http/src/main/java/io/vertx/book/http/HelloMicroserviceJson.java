fb7c87d2305e8e37921f17be4e47e4590c75d124
Pulkit Gupta <pulgupta@gmail.com>
2017-07-15 22:37:56 +0530

    added json

--- hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroserviceJson.java
+++ hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroserviceJson.java
@@ -1,6 +1,8 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
@@ -23,5 +25,9 @@ public class HelloMicroserviceJson extends AbstractVerticle {
         if(rc.pathParam("name") != null) {
             message+= " " + rc.pathParam("name");
         }
+        JsonObject json = new JsonObject().put("message", message);
+        rc.response()
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .end(json.encode());
     }
 }
